065313c348022793e277ce1b902f4b84ea3a910b
ShumsAn <shum-sol@outlook.com>
2024-12-25 12:00:58 +0300

    маршрутзация заданий

--- app/routers/task.py
+++ app/routers/task.py
@@ -34,42 +34,55 @@ async def task_by_id(db: Annotated[Session, Depends(get_db)], task_id: int):
     return task
 
 @router.post('/create')
-async def create_task(db: Annotated[Session, Depends(get_db)],create_task: CreateTask, user_id: int):
-    user = db.scalars(select(Task).where(Task.user_id == user_id))
-
+async def create_task(db: Annotated[Session, Depends(get_db)], create_task: CreateTask, user_id: int):
+    user = db.scalar(select(User).where(User.id == user_id))
     if user is None:
-        return HTTPException(
+        raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail='User was not found'
         )
-    else:
-        db.execute(insert(Task).values(title=create_task.title,
+
+    db.execute(insert(Task).values(title=create_task.title,
                                         content=create_task.content,
                                         priority=create_task.priority,
-                                       user_id=user_id))
-        db.commit()
-        return {
+                                        user_id=user_id,
+                                        slug=slugify(create_task.title)))
+    db.commit()
+    return {
         'status_code': status.HTTP_201_CREATED,
         'transaction': 'Successful'
     }
 
-# @router.put('/update')
-# async def update_task(db: Annotated[Session, Depends(get_db)], update_task: UpdateTask, user_id: int):
-#     task = db.scalars(select(Task).where(Task.user.id == user_id)).all()
-#     if task is None:
-#         return HTTPException(
-#             status_code=status.HTTP_404_NOT_FOUND,
-#             detail='Task was not found'
-#         )
-#
-#     db.execute(update(Task).where(User.id == user_id).values(title=update_task.title,
-#                                                              content=update_task.content,
-#                                                              priority=update_task.priority))
-#     db.commit()
-#     return {
-#         'status_code': status.HTTP_201_CREATED,
-#         'transaction': 'Successful'}
+@router.put('/update')
+async def update_task(db: Annotated[Session, Depends(get_db)], update_task: UpdateTask, user_id: int):
+    task = db.scalars(select(User).where(User.id == user_id)).all()
+    if task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Task was not found'
+        )
+
+    db.execute(update(Task).where(User.id == user_id).values(title=update_task.title,
+                                                             content=update_task.content,
+                                                             priority=update_task.priority,
+                                                             slug=slugify(create_task.title)))
+    db.commit()
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Successful'}
 
 @router.delete('/delete')
-async def delete_task():
-        pass
+async def delete_task(db: Annotated[Session, Depends(get_db)],task_id: int):
+    task = db.scalar(select(Task).where(Task.id == task_id))
+    if task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='task was not found"'
+        )
+
+    db.execute(delete(Task).where(Task.id == task_id))
+    db.commit()
+
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'The task has been deleted!'}
